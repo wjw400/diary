18c3378e39bd66fcb45b48e54e96bd5655287db7
wjw_win10 <605593552@qq.com>
2018-02-13 22:36:43 +0800

    初始化项目

---
+++ src/test/java/com/wjw400/diary/service/Test.java
@@ -0,0 +1,14 @@
+package com.wjw400.diary.service;
+
+/**
+ * 描述 ：
+ *
+ * @author : wjw
+ * @version : v1.00
+ * @Creation Date : 2018/2/13 22:36
+ * @Description :
+ * @update : 修改人，修改时间，修改内容
+ * @see :[相关类/方法]
+ */
+public class Test {
+}
